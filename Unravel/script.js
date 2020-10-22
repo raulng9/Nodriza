8786134b18fd667d1205fec3996075b113e47481
ralph9 <salprez4@gmail.com>
2020-10-23 01:04:29 +0200

    Back to the old version

--- Unravel/script.js
+++ Unravel/script.js
@@ -1,50 +1,147 @@
-function onCvLoaded () {
-    console.log('cv', cv);
-    cv.onRuntimeInitialized = onReady;
-}
-const video = document.getElementById('video');
-const actionBtn = document.getElementById('actionBtn');
-const FPS = 30;
-let stream;
+feather.replace();
+
+const controls = document.querySelector('.controls');
+const cameraOptions = document.querySelector('.video-options>select');
+let video = document.querySelector('video');
+//const canvas = document.querySelector('canvasOutput');
+//const screenshotImage = document.querySelector('img');
+const buttons = [...controls.querySelectorAll('button')];
+let streamStarted = false;
 let streaming = false;
-function onReady () {
-    console.log('ready');
-    let src;
-    let dst;
-    let cap;
 
-    video.controls = true;
-    video.addEventListener('play', start);
-    video.addEventListener('pause', stop);
-    video.addEventListener('ended', stop);
+const [play, pause] = buttons;
 
-    function start () {
-        console.log('playing...');
-        streaming = true;
-        const width = video.width;
-        const height = video.height;
-        src = new cv.Mat(height, width, cv.CV_8UC4);
-        dst = new cv.Mat(height, width, cv.CV_8UC1);
-        cap = new cv.VideoCapture(video);
-        setTimeout(processVideo, 0);
+const constraints = {
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
+  }
+};
+
+cameraOptions.onchange = () => {
+  const updatedConstraints = {
+    ...constraints,
+    deviceId: {
+      exact: cameraOptions.value
     }
+  };
 
-    function stop () {
-        console.log('paused or ended');
-        streaming = false;
-    }
+  startStream(updatedConstraints);
+};
 
-    function processVideo () {
+play.onclick = () => {
+  streamStarted = true;
+  if (streamStarted) {
+    video.play();
+    play.classList.add('d-none');
+    pause.classList.remove('d-none');
+    return;
+  }
+  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+    const updatedConstraints = {
+      ...constraints,
+      deviceId: {
+        exact: cameraOptions.value
+      }
+    };
+    startStream(updatedConstraints);
+  }
+
+  processVideo();
+};
+
+const pauseStream = () => {
+  video.pause();
+  play.classList.remove('d-none');
+  pause.classList.add('d-none');
+  streaming = false;
+};
+
+/*
+const doScreenshot = () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0);
+  screenshotImage.src = canvas.toDataURL('image/webp');
+  screenshotImage.classList.remove('d-none');
+};
+*/
+
+pause.onclick = pauseStream;
+//screenshot.onclick = doScreenshot;
+
+const startStream = async (constraints) => {
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  handleStream(stream);
+};
+
+
+const handleStream = (stream) => {
+  video.srcObject = stream;
+  play.classList.add('d-none');
+  pause.classList.remove('d-none');
+  //screenshot.classList.remove('d-none');
+
+};
+
+
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
+};
+
+getCameraSelection();
+
+
+  let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+  let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+  let cap = new cv.VideoCapture(video);
+
+  const FPS = 30;
+  function processVideo() {
+    try {
+
         if (!streaming) {
+            // clean and stop.
             src.delete();
             dst.delete();
             return;
         }
-        const begin = Date.now();
-        cap.read(src)
+
+
+        //console.log(cap);
+        /*
+        let begin = Date.now();
+
+
+        // start processing.
+
+        cap.read(src);
+
         cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
-        cv.imshow('canvasOutput', dst);
-        const delay = 1000/FPS - (Date.now() - begin);
+        cv.imshow('openCVOutput', dst);
+        // schedule the next one.
+        let delay = 1000/FPS - (Date.now() - begin);
         setTimeout(processVideo, delay);
+        */
+
+    } catch (err) {
+        console.log("error during processing");
+        console.log(err);
     }
-}
+};
+
+// schedule the first one.
+setTimeout(processVideo, 0);
