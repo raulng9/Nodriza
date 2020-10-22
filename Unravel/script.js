7238ee2ee87b1aa950ed527373c2a180f54b1afe
ralph9 <salprez4@gmail.com>
2020-10-22 15:47:22 +0200

    Added starting OpenCV script and commented screenshot button

--- Unravel/script.js
+++ Unravel/script.js
@@ -3,7 +3,7 @@ feather.replace();
 const controls = document.querySelector('.controls');
 const cameraOptions = document.querySelector('.video-options>select');
 const video = document.querySelector('video');
-const canvas = document.querySelector('canvas');
+//const canvas = document.querySelector('canvasOutput');
 //const screenshotImage = document.querySelector('img');
 const buttons = [...controls.querySelectorAll('button')];
 let streamStarted = false;
@@ -52,6 +52,8 @@ play.onclick = () => {
     };
     startStream(updatedConstraints);
   }
+
+  startOpenCV();
 };
 
 const pauseStream = () => {
@@ -98,3 +100,36 @@ const getCameraSelection = async () => {
 };
 
 getCameraSelection();
+
+
+function startOpenCV(){
+  let video = document.getElementById('videoInput');
+  let src = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+  let dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+  let cap = new cv.VideoCapture(video);
+
+  const FPS = 30;
+  function processVideo() {
+    try {
+        if (!streaming) {
+            // clean and stop.
+            src.delete();
+            dst.delete();
+            return;
+        }
+        let begin = Date.now();
+        // start processing.
+        cap.read(src);
+        cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+        cv.imshow('openCVOutput', dst);
+        // schedule the next one.
+        let delay = 1000/FPS - (Date.now() - begin);
+        setTimeout(processVideo, delay);
+    } catch (err) {
+        utils.printError(err);
+    }
+};
+
+// schedule the first one.
+setTimeout(processVideo, 0);
+}
